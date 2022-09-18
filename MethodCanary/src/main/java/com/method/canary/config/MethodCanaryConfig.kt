7e4f11cf61a49dd2561837de648b507ef7b0125d
ningtukun@xa.com <ningtukun@xa.com>
2022-09-18 23:21:16 +0800

    监听Handler消息的前后处理

--- MethodCanary/src/main/java/com/method/canary/config/MethodCanaryConfig.kt
+++ MethodCanary/src/main/java/com/method/canary/config/MethodCanaryConfig.kt
@@ -6,5 +6,5 @@ package com.method.canary.config
 object MethodCanaryConfig {
 
     const val DEFAULT_EVIL_METHOD_THRESHOLD_MS = 700//方法耗时阈值，超过此阈值,判定为异常耗时方法
-
+    const val TAG = " MethodCanary"
 }
--- MethodCanary/src/main/java/com/method/canary/monitor/LooperMonitor.kt
+++ MethodCanary/src/main/java/com/method/canary/monitor/LooperMonitor.kt
@@ -2,6 +2,12 @@ package com.method.canary.monitor
 
 import android.os.Looper
 import android.os.MessageQueue
+import android.util.Printer
+import com.method.canary.config.MethodCanaryConfig
+import com.method.canary.config.MethodCanaryConfig.TAG
+import com.method.canary.util.MatrixLog
+import com.method.canary.util.ReflectUtils
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 /**
@@ -9,20 +15,82 @@ import java.util.concurrent.ConcurrentHashMap
  */
 class LooperMonitor : MessageQueue.IdleHandler {
 
-    private val sLooperMonitorMap = ConcurrentHashMap<Looper, LooperMonitor>()
-    private val sMainMonitor: LooperMonitor = LooperMonitor.of(Looper.getMainLooper())
+    companion object {
+        private val LOOPER_MONITOR_MAP = ConcurrentHashMap<Looper, LooperMonitor>()
+        private val MAIN_MONITOR = of(Looper.getMainLooper())
+
+        private const val isReflectLoggingError = false//标识反射获取消息打印器是否失败
+
+        //根据当前的looper构造对应的LooperMonitor
+        fun of(looper: Looper): LooperMonitor {
+            return LOOPER_MONITOR_MAP[looper] ?: run {
+                val looperMonitor = LooperMonitor(looper)
+                LOOPER_MONITOR_MAP[looper] = looperMonitor
+                looperMonitor
+            }
+        }
+    }
+
+    private var looper: Looper
+    private lateinit var printer: LooperPrinter
+
+    private constructor(looper: Looper) {
+        Objects.requireNonNull(looper)
+        this.looper = looper
+        resetPrinter()
+        addIdleHandler(looper)
+    }
+
+    @Synchronized
+    private fun resetPrinter() {
+        var originPrinter: Printer? = null
+        try {
+            if (!isReflectLoggingError) {
+                originPrinter = ReflectUtils.get(looper.javaClass, "mLogging", looper)
+                if (originPrinter == printer){
+
+                }
+            }
+        }catch (e:Exception){
+
+        }
+    }
+
 
     override fun queueIdle(): Boolean {
         return true
     }
 
-    fun of(looper: Looper): LooperMonitor? {
-        var looperMonitor = LooperMonitor.sLooperMonitorMap.get(looper)
-        if (looperMonitor == null) {
-            looperMonitor = LooperMonitor(looper)
-            LooperMonitor.sLooperMonitorMap.put(looper, looperMonitor)
-        }
-        return looperMonitor
+    //监听消息的分发
+    private fun dispatch(isBegin:Boolean, log:String?){
+
     }
+
+    inner class LooperPrinter(private val originPrinter: Printer?) : Printer {
+        var isHasChecked = false
+        var isValid = false
+
+        override fun println(x: String?) {
+            originPrinter?.println(x)//业务方或其他sdk设置的消息打印器
+            if (originPrinter == this) {
+                throw RuntimeException("${MethodCanaryConfig.TAG} origin == this")
+            }
+
+            //检查消息打印机制的规则是否有效，有效才去做监听，因为有可能android版本更新，这套机制可能会失效
+            if (!isHasChecked) {
+                val firstChar = x?.firstOrNull()
+                isValid = firstChar == '>' || firstChar == '<'
+                if (!isValid) {//消息打印规则跟我们的预设不一样
+                    MatrixLog.e(TAG, "[println] Printer is inValid! x:%s", x)
+                }
+            }
+
+            if (isValid) {//消息打印规则跟我们的预设匹配，监听消息的开始和结束
+                dispatch(x?.firstOrNull() == '>', x)
+            }
+        }
+    }
+
+
 
 }
