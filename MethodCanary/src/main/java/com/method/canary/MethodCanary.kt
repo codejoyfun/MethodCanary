d86a2bd683cc284f3179b2d7072c5de1881aa914
ningtukun@xa.com <ningtukun@xa.com>
2022-09-21 21:11:49 +0800

    新增AppMethodBeat类

---
+++ MethodCanary/src/main/java/com/method/canary/MethodCanary.kt
@@ -0,0 +1,16 @@
+package com.method.canary
+
+/**
+ * 创建时间：2022/9/21
+ * 编写人：ningtukun
+ * 功能描述：
+ */
+object MethodCanary {
+
+    fun start(){
+        val runnable = Runnable {
+
+        }
+    }
+
+}
